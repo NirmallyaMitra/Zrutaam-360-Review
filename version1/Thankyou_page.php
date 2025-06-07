8360d8cc29272e812de549529d45fe412b5b2549
NirmallyaMitra <nirmallyamitra.github@gmail.com>
2025-06-07 13:49:01 +0530

    message

--- version1/Thankyou_page.php
+++
@@ -1,3 +0,0 @@
-<?php
-echo "Thank You For Taking The Review";
-?>
--- version1/assets/JS/leaders.js
+++
@@ -1,9 +0,0 @@
-const option = document.getElementById("gendergroup");
-const inputbox = document.getElementById("self-1");
-option.onchange = function () {
-  if (option.value == "self-describe") {
-    inputbox.style.display = "block";
-  } else {
-    inputbox.style.display = "none";
-  }
-};
